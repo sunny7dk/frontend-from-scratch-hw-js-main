00924e14a39a0db754c8e0d1ff5ffd06405d05fd
sunny7dk <dosandusaliev5@gmail.com>
2025-06-12 18:28:02 +0500

    sixth

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,11 +1,11 @@
+const number = 10;
 let factorial = 1;
 let i = 1;
 
-while (i <= 10) {
+while (i <= number) {
 
   factorial *= i;
   i++;
-  console.log(factorial);
 }
 
 console.log(factorial);
