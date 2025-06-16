cfc2d12bab7c0ff8c57c4b10d8863f464b16255a
sunny7dk <dosandusaliev5@gmail.com>
2025-06-16 19:36:07 +0500

    lesson-01/03--first

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -5,3 +5,17 @@
 */
 
 // your code
+
+const number = 20;
+const divider = 2;
+let sum = 0;
+
+let i = 0;
+
+while (i < number) {
+    if (i % divider === 1) {
+    sum = i;
+    console.log(sum);
+    }
+    i++;
+}
