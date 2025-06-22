194ecaec9456e3ba97a3b91bb3ff0cb1e8964e6e
sunny7dk <dosandusaliev5@gmail.com>
2025-06-22 14:12:23 +0500

    lesson-03/02--2

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,10 +1,16 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-const a = 2;
+const a = 6;
 function isEven(num_a) {
-    c = num_a % 2 === 0;
-    return c;
+    let b;
+    if (num_a % 2 === 0) {
+        b = true;
+    } else {
+        b = false;
+    }
+
+    return b;
 }
 
 let result = isEven(a);
