72ad8598cd44cfe421284e775594efadaf8c0dd3
sunny7dk <dosandusaliev5@gmail.com>
2025-08-09 10:03:56 +0500

    lesson-06/01.js

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -23,4 +23,83 @@ const oddNumbers = filter(numbers, (element, index) => {
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
 
-const filter = () => {}
+const numbers = [1, 2, 3, 4, 5];
+
+const filter = (num, callBack) => {
+  let newFilter = [];
+  for (let i = 0; i < num.length; i++) {
+    if (callBack(num[i], i)) {
+      newFilter.push(num[i]);
+    }
+  }
+  return newFilter;
+}
+
+const result = filter(numbers, (element, index) => {
+  return element % 2 !== 0;
+});
+
+console.log(result);
+
+
+
+
+
+
+
+// const myBooks = [
+//     {
+//         id: 1,
+//         title: 'Head First JavaScript Programming',
+//         author: 'Eric Freeman',
+//         price: 29.99,
+//         genre: 'Programming',
+//     },
+//     {
+//         id: 2,
+//         title: 'You Don’t Know JS',
+//         author: 'Kyle Simpson',
+//         price: 39.99,
+//         genre: 'Programming',
+//     },
+//     {
+//         id: 3,
+//         title: 'JavaScript: The Definitive Guide',
+//         author: 'David Flanagan',
+//         price: 49.99,
+//         genre: 'Programming',
+//     },
+//     {
+//         id: 4,
+//         title: 'Lord of the Rings',
+//         author: 'J.R.R. Tolkien',
+//         price: 29.99,
+//         genre: 'Fantasy',
+//     },
+//     {
+//         id: 5,
+//         title: 'The Witcher',
+//         author: 'Andrzej Sapkowski',
+//         price: 19.99,
+//         genre: 'Fantasy',
+//     },
+// ];
+
+// const filter = (books, callBack) => {
+//   let newFilter = [];
+//   for (let i = 0; i < books.length; i++) {
+//     if (callBack(books[i].price, i)) {
+//       newFilter.push(books[i].price);
+//     }
+//   }
+// }
+
+// const result = filter(myBooks, callBack(element, index) => {
+  
+// })
+
+
+
+
+
+
